2e479708ac104b08ece3b4087f538df6ae97f112
Vedant23258 <akashboi40@gmail.com>
2026-02-12 16:14:49 +0530

    WELCOME NEW USER

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,11 @@
+
+public class PalindromeCheckerApp{
+
+    public static void main(String[] args){
+
+
+        System.out.println("Welcome to the palindrome checker");
+        System.out.println("version 1.0");
+        System.out.println("system");
+    }
+}
