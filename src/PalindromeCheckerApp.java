6a1e439205615a440d5ea8cd172594d3d315ebb0
Vedant Mishra <mishravedant90@gmail.com>
2026-03-06 15:34:01 +0530

    Refactor PalindromeCheckerApp with welcome message
    Updated the PalindromeCheckerApp class to include a welcome message and version information.

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,24 +1,7 @@
-import java.util.Queue;
-import java.util.LinkedList;
-import java.util.Stack;
-
-public class UseCase6PalindromeCheckerApp {
-    public static void main(String[] args) {
-        String input = "civic";
-        Queue<Character> queue = new LinkedList<>();
-        Stack<Character> stack = new Stack<>();
-        for (char c : input.toCharArray()) {
-            queue.add(c);      // Enqueue (FIFO)
-            stack.push(c);     // Push (LIFO)
-        }
-        boolean isPalindrome = true;
-        while (!queue.isEmpty()) {
-            if (!queue.remove().equals(stack.pop())) {
-                isPalindrome = false;
-                break;
-            }
-        }
-        System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome);
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to the Palindrome Checker Managment System");
+        System.out.println("Version : 1.0");
+        System.out.println("System intialized successfully");
     }
 }
