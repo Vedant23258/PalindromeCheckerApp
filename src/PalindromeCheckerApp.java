92164f0a665d811a8c2b39d728e8046fe349c9a5
Vedant Mishra <mishravedant90@gmail.com>
2026-03-06 15:12:04 +0530

    Refactor palindrome checker logic and variable names

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,18 +1,15 @@
 
-public class PalindromeCheckerApp{
-
+public class PalindromeCheckerApp {
     public static void main(String[] args){
-
-        String input = "madam";
-        boolean isPalindrome = true;
-
-        for (int i = 0; i < input.length() / 2; i++) {
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-                isPalindrome = false;
+        boolean ispalindrome = true;
+        String text = "madam";
+        for (int i =0;i<text.length()/2;i++){
+            if(text.charAt(i) != text.charAt(text.length()-1-i)){
+                ispalindrome = false;
                 break;
             }
         }
-        System.out.println("Input text: " + input);
-        System.out.println("Is it a Palindrome? : " + isPalindrome);
+        System.out.println("Input Text:"+text);
+        System.out.println("Is it a Palindrome? " + ispalindrome);
     }
 }
